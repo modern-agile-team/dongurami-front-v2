3ba4d01203a2164564d998b2e21e124a193f150c
soonki-98 <alstnsrl98@gmail.com>
2023-11-13 23:07:12 +0900

    feat(#2): 테마 추가

---
+++ src/styles/color.ts
@@ -0,0 +1,13 @@
+/*
+ * Created on Mon Nov 13 2023
+ *
+ * Copyright (c) 2023 Your Company
+ */
+
+import { Theme } from "@emotion/react";
+
+const colorTheme: Theme["color"] = {
+  primary: "#fff",
+};
+
+export default colorTheme;
---
+++ src/styles/font.ts
@@ -0,0 +1,17 @@
+/*
+ * Created on Mon Nov 13 2023
+ *
+ * Copyright (c) 2023 Your Company
+ */
+
+import { Theme } from "@emotion/react";
+
+const fontTheme: Theme["font"] = {
+  Body1: {
+    fontSize: "16px",
+    fontWeight: "bold",
+    lineHeight: "24px",
+  },
+};
+
+export default fontTheme;
