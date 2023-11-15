40f9cd3c2c64dcfa407083e6e908fd90ebd6ae5f
soonki-98 <alstnsrl98@gmail.com>
2023-11-15 11:48:43 +0900

    feat: api folder

---
+++ src/apis/board.ts
@@ -0,0 +1,11 @@
+/*
+ * Created on Wed Nov 15 2023
+ *
+ * Copyright (c) 2023 Your Company
+ */
+
+import instance from "./instance";
+
+export const getBoard = () => {
+  return instance.get("");
+};
---
+++ src/apis/instance.ts
@@ -0,0 +1,17 @@
+/*
+ * Created on Wed Nov 15 2023
+ *
+ * Copyright (c) 2023 Your Company
+ */
+
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: "",
+  headers: {
+    "x-auth-token": "token",
+    "api-key": process.env.NEXT_PUBLIC_API_KEY,
+  },
+});
+
+export default instance;
