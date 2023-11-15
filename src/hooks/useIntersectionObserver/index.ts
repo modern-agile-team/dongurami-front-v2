35fab35510ba2735d234926b41e94534cc128f78
soonki-98 <alstnsrl98@gmail.com>
2023-11-15 11:35:08 +0900

    feat: useIntersectionObserver

---
+++ src/hooks/useIntersectionObserver/index.ts
@@ -0,0 +1,7 @@
+/*
+ * Created on Wed Nov 15 2023
+ *
+ * Copyright (c) 2023 Your Company
+ */
+
+export { default as useIntersectionObserver } from "./useIntersectionObserver";
---
+++ src/hooks/useIntersectionObserver/useIntersectionObserver.ts
@@ -0,0 +1,47 @@
+/*
+ * Created on Wed Nov 15 2023
+ *
+ * Copyright (c) 2023 Your Company
+ */
+
+import { useEffect, useRef, useState } from "react";
+
+/**
+ * @param callback 화면상에 표시됐을때 실행할 함수.
+ * @param dependencies 의존성 배열
+ * @param options IntersectionObserver 옵션
+ * @returns ```tsx
+ * 	type observerRef = (node: Element) => void; // 감지를 원하는 dom에 주입할 ref
+ *  type isShow = boolean; // 해당 dom이 화면에 렌더됐는지 여부
+ *
+ * 	examples
+ *  <li ref={index === list.length - 1 ? observerRef : null}/>
+ * ```
+ */
+export default function useIntersectionObserver<E extends HTMLElement>(
+  callback?: (node: IntersectionObserverEntry) => void,
+  dependencies: React.DependencyList = [],
+  options: IntersectionObserverInit = { threshold: 1, root: null, rootMargin: "0px 0px 0px 0px" }
+) {
+  const observerRef = useRef<E>(null);
+  const [isShow, setIsShow] = useState(false);
+
+  useEffect(() => {
+    const target = observerRef.current;
+    if (!target) return;
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        setIsShow(entries[0].isIntersecting);
+        callback && callback(entries[0]);
+      },
+      { ...options }
+    );
+    observer.observe(target);
+    return () => {
+      observer.unobserve(target);
+    };
+  }, [observerRef.current, ...dependencies]);
+
+  return { observerRef, isShow };
+}
