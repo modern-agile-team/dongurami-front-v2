7afa31d9d8eccc046a74acfaedf060240c9989f5
soonki-98 <alstnsrl98@gmail.com>
2023-11-16 18:30:42 +0900

    chore: tsconfig for test

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,10 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
+  globals: {
+    "ts-jest": {
+      tsconfig: "./tsconfig.test.json",
+    },
+  },
   preset: "ts-jest",
   testEnvironment: "jsdom",
   transformIgnorePatterns: ["/node_modules/(?!node-fetch/.*)"],
