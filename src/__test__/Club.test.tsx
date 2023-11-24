2b4a9434db5660b218b1bb1fa35afac1aa1a615e
soonki-98 <alstnsrl98@gmail.com>
2023-11-24 10:56:42 +0900

    test(#20): 동아리홈 테스트 작성

---
+++ src/__test__/Club.test.tsx
@@ -0,0 +1,38 @@
+import { render, screen } from "@testing-library/react";
+import mockRouter from "next-router-mock";
+import { RouterContext } from "next/dist/shared/lib/router-context.shared-runtime";
+import userEvent from "@testing-library/user-event";
+
+import { DonguramiProvider } from "@/components";
+import ClubPage from "@/pages/club/[clubID]";
+
+describe("동아리 페이지 테스트", () => {
+  beforeEach(() => {
+    mockRouter.setCurrentUrl("/club/[clubID]");
+    mockRouter.query = { clubID: "1" };
+  });
+  it("동아리 페이지에서는 홈이 먼저 표시된다.", async () => {
+    render(
+      <RouterContext.Provider value={mockRouter}>
+        <DonguramiProvider>
+          <ClubPage />
+        </DonguramiProvider>
+      </RouterContext.Provider>
+    );
+
+    expect(mockRouter.query.tab).toBe("home");
+  });
+
+  it("탭 전환 버튼을 클릭하면 쿼리스트링이 바뀐다.", async () => {
+    render(
+      <RouterContext.Provider value={mockRouter}>
+        <DonguramiProvider>
+          <ClubPage />
+        </DonguramiProvider>
+      </RouterContext.Provider>
+    );
+    const noticeButton = await screen.findByRole("button", { name: "공지" });
+    await userEvent.click(noticeButton);
+    expect(mockRouter.query.tab).toBe("notice");
+  });
+});
