2fa1c06145ffffc6109db65f3b55272a58177552
soonki-98 <alstnsrl98@gmail.com>
2023-11-24 11:47:21 +0900

    test(#20): 동아리 잘못된 탭 접근 테스트 추가

--- src/__test__/Club.test.tsx
+++ src/__test__/Club.test.tsx
@@ -10,7 +10,7 @@ describe("동아리 페이지 테스트", () => {
     mockRouter.setCurrentUrl("/club/[clubID]");
     mockRouter.query = { clubID: "1" };
   });
-  it("동아리 페이지에서는 홈이 먼저 표시된다.", async () => {
+  it("1. 동아리 페이지에서는 홈이 먼저 표시된다.", async () => {
     render(
       <TestProvider router={mockRouter}>
         <ClubPage />
@@ -20,7 +20,7 @@ describe("동아리 페이지 테스트", () => {
     expect(mockRouter.query.tab).toBe("home");
   });
 
-  it("탭 전환 버튼을 클릭하면 쿼리스트링이 바뀐다.", async () => {
+  it("2. 탭 전환 버튼을 클릭하면 쿼리스트링이 바뀐다.", async () => {
     render(
       <TestProvider router={mockRouter}>
         <ClubPage />
@@ -29,5 +29,15 @@ describe("동아리 페이지 테스트", () => {
     const noticeButton = await screen.findByRole("button", { name: "공지" });
     await userEvent.click(noticeButton);
     expect(mockRouter.query.tab).toBe("notice");
+  });
+
+  it("3. 잘못된 탭으로 접근시 home 탭으로 이동한다.", async () => {
+    mockRouter.query = { ...mockRouter.query, tab: "wrong_tab" };
+    render(
+      <TestProvider router={mockRouter}>
+        <ClubPage />
+      </TestProvider>
+    );
+    expect(mockRouter.query.tab).toBe("home");
   });
 });
