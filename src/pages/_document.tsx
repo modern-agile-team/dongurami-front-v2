16a8c5925c84abcab5594a54c71cdd5dc7ca013f
soonki-98 <alstnsrl98@gmail.com>
2023-11-27 16:23:10 +0900

    https to http test

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta
+          http-equiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
