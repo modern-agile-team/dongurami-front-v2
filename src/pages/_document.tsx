425586c9918c9a8687376fc0779d041732f895ef
soonki-98 <alstnsrl98@gmail.com>
2023-11-27 16:27:04 +0900

    rollback

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,12 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <meta
-          http-equiv="Content-Security-Policy"
-          content="upgrade-insecure-requests"
-        />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
