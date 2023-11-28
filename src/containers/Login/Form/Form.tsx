50d9c5fe6d7750c29905054ac8f6485862a6c4ed
soonki-98 <alstnsrl98@gmail.com>
2023-11-28 20:09:14 +0900

    feat(#QYOG-12): 아이디 패스워드 입력과 submit 기능 추가

--- src/containers/Login/Form/Form.tsx
+++ src/containers/Login/Form/Form.tsx
@@ -4,20 +4,51 @@
  * Copyright (c) 2023 Your Company
  */
 
+import React, { useState } from "react";
+
 import { Column } from "@/components";
+import * as S from "./emotion";
 
 export default function Form() {
+  const [loginInfo, setLoginInfo] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleChange = (ev: React.ChangeEvent<HTMLInputElement>) => {
+    const id = ev.target.id as "username" | "password";
+
+    setLoginInfo({ ...loginInfo, [id]: ev.target.value });
+  };
+
+  const handleLoginSubmit = (ev: React.FormEvent<HTMLFormElement>) => {
+    ev.preventDefault();
+    alert(`아이디 ${loginInfo.username}\n비밀번호 ${loginInfo.password}`);
+  };
+
   return (
-    <Column.form gap={10}>
-      <Column>
+    <S.FormLayout gap={10} onSubmit={handleLoginSubmit}>
+      <Column.label>
         <span>아이디</span>
-        <input type="text" placeholder="아이디를 입력하세요" />
-      </Column>
-      <Column>
+        <input
+          id="username"
+          onChange={handleChange}
+          value={loginInfo.username}
+          type="text"
+          placeholder="아이디를 입력하세요"
+        />
+      </Column.label>
+      <Column.label>
         <span>비밀번호</span>
-        <input type="text" placeholder="비밀번호를 입력하세요" />
-      </Column>
+        <input
+          id="password"
+          onChange={handleChange}
+          value={loginInfo.password}
+          type="text"
+          placeholder="비밀번호를 입력하세요"
+        />
+      </Column.label>
       <button>로그인</button>
-    </Column.form>
+    </S.FormLayout>
   );
 }
