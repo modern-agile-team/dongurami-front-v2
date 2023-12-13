c2109f22a1628599a01d536420097d540a0ac9ee
soonki-98 <alstnsrl98@gmail.com>
2023-12-13 15:30:17 +0900

    feat(QYOG-32): 자유게시판 무한스크롤로 임시 적용

--- src/pages/board/infinity/index.tsx
+++ src/pages/board/infinity/index.tsx
@@ -4,8 +4,25 @@
  * Copyright (c) 2023 Your Company
  */
 
+import { boardsAPI } from "@/apis";
 import { InfinityScrollBoard } from "@/containers/Board";
 
-export default function InfinityTestPage() {
-  return <InfinityScrollBoard />;
+export default function InfinityTestPage(props: {
+  freeBoard: Swagger.Api.FreePostFindAllAndCount.ResponseBody;
+}) {
+  return <InfinityScrollBoard {...props} />;
+}
+
+export async function getServerSideProps() {
+  const freeBoard = await boardsAPI.freeBoard.getAll({
+    id: 1,
+    page: 1,
+    pageSize: 20,
+  });
+
+  return {
+    props: {
+      freeBoard,
+    },
+  };
 }
