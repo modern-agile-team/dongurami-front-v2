e2e04a2106eee069be8245c5860dc8d239ba5211
soonki-98 <alstnsrl98@gmail.com>
2023-12-15 22:38:39 +0900

    feat(QYOG-32): Pagination 컴포넌트 생성

---
+++ src/components/UI/Pagination/Pagination.tsx
@@ -0,0 +1,78 @@
+/*
+ * Created on Thu Dec 14 2023
+ *
+ * Copyright (c) 2023 Your Company
+ */
+
+import { Button, Row, WhatIF } from "@/components";
+import React, { useMemo, useState } from "react";
+
+interface IPaginationProps {
+  count: number;
+  defaultPage: number;
+  siblingCount: number;
+  boundaryCount: number;
+  onChange: (ev: React.ChangeEvent<unknown>, page: number) => void;
+}
+
+/**
+ *
+ * @param defaultPage 최초 페이지
+ * @param siblingCount 현재 페이지 기준 앞 뒤로 더 보여줄 페이지 갯수
+ * @param boundaryCount 맨 앞과 맨 뒤에 보여줄 페이지 갯수
+ */
+export default function Pagination({
+  defaultPage = 1,
+  siblingCount = 1,
+  boundaryCount = 1,
+  ...props
+}: IPaginationProps) {
+  const [currentPage, setCurrentPage] = useState(defaultPage);
+
+  const totalList = useMemo(() => {
+    const arr = Array.from({ length: props.count }, (_, index) => index + 1);
+    const filteredArr = arr.map((page) => {
+      if (
+        page <= boundaryCount ||
+        page + boundaryCount > props.count ||
+        (currentPage >= page - siblingCount &&
+          currentPage <= page + siblingCount)
+      ) {
+        return `${page}`;
+      }
+      if (page < currentPage) return "skipPrev";
+      return "skipNext";
+    });
+
+    return [...new Set(filteredArr)];
+  }, [props.count, currentPage]);
+
+  return (
+    <Row.ul gap={4}>
+      {totalList.map((page) => {
+        return (
+          <Row.li key={`pagination-${page}`}>
+            <WhatIF
+              condition={page !== "skipPrev" && page !== "skipNext"}
+              falsy={<>...</>}
+            >
+              <Button
+                css={{
+                  backgroundColor:
+                    `${page}` === `${currentPage}` ? "red" : "blue",
+                }}
+                disabled={`${currentPage}` === `${page}`}
+                onClick={(ev) => {
+                  setCurrentPage(Number(page));
+                  props.onChange(ev, Number(page));
+                }}
+              >
+                {page}
+              </Button>
+            </WhatIF>
+          </Row.li>
+        );
+      })}
+    </Row.ul>
+  );
+}
