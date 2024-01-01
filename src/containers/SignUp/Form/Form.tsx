dd7bdbf3bbaa6dbbe1f86dc808fb0c7efec0e841
Hyunwoo <pho9902@naver.com>
2024-01-01 15:02:16 +0900

    feat(QYOG-37): session provider 추가, useSession 테스트

--- src/containers/SignUp/Form/Form.tsx
+++ src/containers/SignUp/Form/Form.tsx
@@ -6,8 +6,12 @@
 
 import { Column } from "@/components";
 import * as S from "./emotion";
+import { signIn, useSession } from "next-auth/react";
 
 export default function Form() {
+  const { data, status } = useSession();
+  console.log(data, status);
+
   return (
     <S.FormLayout>
       <Column.h2>회원가입</Column.h2>
