84c1b3e153c364a5be7bda057af409abb729b6c3
Hyunwoo <pho9902@naver.com>
2024-01-02 11:42:42 +0900

    feat(QYOG-37): 테스트 파일 더미 session 추가

--- src/__test__/Club.test.tsx
+++ src/__test__/Club.test.tsx
@@ -1,18 +1,22 @@
 import { render, screen } from "@testing-library/react";
 import mockRouter from "next-router-mock";
 import userEvent from "@testing-library/user-event";
+import { Session } from "next-auth";
 
 import ClubPage from "@/pages/club/[clubID]";
 import { TestProvider } from "./utils";
 
 describe("동아리 페이지 테스트", () => {
+  const session: Session = {
+    expires: "ISODateString",
+  };
   beforeEach(() => {
     mockRouter.setCurrentUrl("/club/[clubID]");
     mockRouter.query = { clubID: "1" };
   });
   it("1. 동아리 페이지에서는 홈이 먼저 표시된다.", async () => {
     render(
-      <TestProvider router={mockRouter}>
+      <TestProvider router={mockRouter} session={session}>
         <ClubPage />
       </TestProvider>
     );
@@ -22,7 +26,7 @@ describe("동아리 페이지 테스트", () => {
 
   it("2. 탭 전환 버튼을 클릭하면 쿼리스트링이 바뀐다.", async () => {
     render(
-      <TestProvider router={mockRouter}>
+      <TestProvider router={mockRouter} session={session}>
         <ClubPage />
       </TestProvider>
     );
@@ -34,7 +38,7 @@ describe("동아리 페이지 테스트", () => {
   it("3. URL을 통해 잘못된 탭으로 접근시 home 탭으로 이동한다.", async () => {
     mockRouter.query = { ...mockRouter.query, tab: "wrong_tab" };
     render(
-      <TestProvider router={mockRouter}>
+      <TestProvider router={mockRouter} session={session}>
         <ClubPage />
       </TestProvider>
     );
