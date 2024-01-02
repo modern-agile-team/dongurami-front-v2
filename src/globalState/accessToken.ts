299f335e8ed9a0a40dccbd2b8218b0494f2d39ff
soonki-98 <alstnsrl98@gmail.com>
2024-01-02 10:25:33 +0900

    feat(QYOG-42): atomWithStorage storage option 추가

--- src/globalState/accessToken.ts
+++ src/globalState/accessToken.ts
@@ -8,6 +8,16 @@ import { atomWithStorage } from "jotai/utils";
 
 type AccessToken = string | null;
 
-const accessTokenAtom = atomWithStorage<AccessToken>("an", null);
+const accessTokenAtom = atomWithStorage<AccessToken>("an", null, {
+  getItem(key, initialValue) {
+    return localStorage.getItem(key) || initialValue;
+  },
+  setItem(key, newValue) {
+    localStorage.setItem(key, `${newValue}`);
+  },
+  removeItem(key) {
+    localStorage.removeItem(key);
+  },
+});
 
 export default accessTokenAtom;
