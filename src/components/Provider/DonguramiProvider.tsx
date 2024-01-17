a079865724d9c00195f1cb6ae57c9717efbf5ff9
soonki-98 <alstnsrl98@gmail.com>
2024-01-17 16:32:26 +0900

    refactor(QYOG-41): theme 관련 파일 폴더 구조 수정

--- src/components/Provider/DonguramiProvider.tsx
+++ src/components/Provider/DonguramiProvider.tsx
@@ -9,18 +9,22 @@ import { Provider, createStore } from "jotai";
 import { Theme, ThemeProvider } from "@emotion/react";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
-import typoTheme from "@/styles/font";
-import { darkThemeColor, lightThemeColor } from "@/styles/color";
+import {
+  typographyTheme,
+  lightThemeColor,
+  darkThemeColor,
+} from "@/styles/theme";
 
 import ErrorBoundary from "./ErrorBoundary";
 import { SessionProvider } from "next-auth/react";
 import { Session } from "next-auth";
+
 const theme: Theme = {
   color: {
     light: lightThemeColor,
     dark: darkThemeColor,
   },
-  typography: typoTheme,
+  typography: typographyTheme,
 };
 
 const queryClient = new QueryClient({
