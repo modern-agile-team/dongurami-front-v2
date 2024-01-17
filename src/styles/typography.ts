db324a439c789b52245347982deb5d59c73be4f5
soonki-98 <alstnsrl98@gmail.com>
2024-01-17 16:29:53 +0900

    feat(QYOG-41): typography theme 지정

---
+++ src/styles/typography.ts
@@ -0,0 +1,72 @@
+/*
+ * Created on Mon Nov 13 2023
+ *
+ * Copyright (c) 2023 Your Company
+ */
+
+import { Theme } from "@emotion/react";
+
+const typography: Theme["typography"] = {
+  Head1: {
+    fontSize: "96px",
+    lineHeight: "normal",
+    fontWeight: 700,
+  },
+  Head2: {
+    fontSize: "60px",
+    lineHeight: "normal",
+    fontWeight: 700,
+  },
+  Head3: {
+    fontSize: "48px",
+    lineHeight: "normal",
+    fontWeight: 700,
+  },
+  Head4: {
+    fontSize: "34px",
+    lineHeight: "normal",
+    fontWeight: 600,
+  },
+  Head5: {
+    fontSize: "24px",
+    lineHeight: "normal",
+    fontWeight: 600,
+  },
+  Head6: {
+    fontSize: "21px",
+    lineHeight: "normal",
+    fontWeight: 500,
+  },
+  SubTitle1: {
+    fontSize: "17px",
+    lineHeight: "normal",
+    fontWeight: 400,
+  },
+  SubTitle2: {
+    fontSize: "15px",
+    lineHeight: "normal",
+    fontWeight: 400,
+  },
+  Body1: {
+    fontSize: "16px",
+    lineHeight: "normal",
+    fontWeight: 500,
+  },
+  Body2: {
+    fontSize: "14px",
+    lineHeight: "normal",
+    fontWeight: 500,
+  },
+  Button: {
+    fontSize: "14px",
+    lineHeight: "normal",
+    fontWeight: 300,
+  },
+  Caption: {
+    fontSize: "12px",
+    lineHeight: "normal",
+    fontWeight: 200,
+  },
+};
+
+export default typography;
