726d7d3042ffe5eda80001e86018c533ec4348b9
soonki-98 <alstnsrl98@gmail.com>
2024-01-17 16:53:59 +0900

    chore(QYOG-41): 주석 수정

--- src/components/Utilities/Typography/Typography.tsx
+++ src/components/Utilities/Typography/Typography.tsx
@@ -1,21 +1,9 @@
-/**
- * @copyright (C) 2023, rsupport. All rights reserved
+/*
+ * Created on Wed Jan 17 2024
  *
- * @license
- * This software and/or source code may be used, copied and/or disseminated only
- * with the written permission of rsupport, or in accordance with the terms
- * and conditions stipulated in the agreement/contract under which the software
- * and/or source code has been supplied by rsupport or its affiliates.
- * Unauthorized use, copying, or dissemination of this file, via any medium, is
- * strictly prohibited, and will constitute an infringement of copyright.
- *
- * @author
- * Soonki Min (skmin@rsupport.com)
- *
- * @description
- *
- * @created at Fri Mar 24 2023
- **/
+ * Copyright (c) 2024 Your Company
+ */
+
 import React, { AllHTMLAttributes, forwardRef } from "react";
 import { Theme, css } from "@emotion/react";
 import styled from "@emotion/styled";
