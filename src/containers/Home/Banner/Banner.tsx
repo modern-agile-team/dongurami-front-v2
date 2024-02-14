fd918b29181b911264c1c0bf11399d0b174b0127
soonki-98 <alstnsrl98@gmail.com>
2024-02-15 00:08:50 +0900

    feat(QYOG-79): 동아리 추천받기 버튼 클릭시 라우팅 (동아리 추천 페이지가 아직 없어 루트로 임시 작업했습니다.)

--- src/containers/Home/Banner/Banner.tsx
+++ src/containers/Home/Banner/Banner.tsx
@@ -4,16 +4,26 @@
  * Copyright (c) 2024 Your Company
  */
 
-import { Button, Column, Typography } from "@/components";
+import { useRouter } from "next/router";
+
+import { Button, Typography } from "@/components";
 import * as S from "./emotion";
 
 export default function Banner() {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push("/");
+  };
+
   return (
     <S.Wrapper gap={27}>
       <Typography typoSize="Head3" typoColor="primary_100">
         아직도 동아리를 찾고 있다면?
       </Typography>
-      <Button size="xl">동아리 추천받기</Button>
+      <Button size="xl" onClick={handleClick}>
+        동아리 추천받기
+      </Button>
     </S.Wrapper>
   );
 }
