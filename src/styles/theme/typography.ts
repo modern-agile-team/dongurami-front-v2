536fc2f2bddafb7e2fbc0216c5456e32fa67c6b0
soonki-98 <alstnsrl98@gmail.com>
2024-02-14 23:55:19 +0900

    feat(QYOG-79): typograhpy 수정

--- src/styles/theme/typography.ts
+++ src/styles/theme/typography.ts
@@ -8,64 +8,64 @@ import { Theme } from "@emotion/react";
 
 const typography: Theme["typography"] = {
   Head1: {
-    fontSize: "96px",
+    fontSize: "6rem",
     lineHeight: "normal",
     fontWeight: 700,
   },
   Head2: {
-    fontSize: "60px",
+    fontSize: "3.75rem",
     lineHeight: "normal",
     fontWeight: 700,
   },
   Head3: {
-    fontSize: "48px",
+    fontSize: "3px",
     lineHeight: "normal",
     fontWeight: 700,
   },
   Head4: {
-    fontSize: "34px",
+    fontSize: "2.125rem",
     lineHeight: "normal",
     fontWeight: 600,
   },
   Head5: {
-    fontSize: "24px",
+    fontSize: "1.5rem",
     lineHeight: "normal",
     fontWeight: 600,
   },
   Head6: {
-    fontSize: "21px",
+    fontSize: "1.3125rem",
     lineHeight: "normal",
     fontWeight: 500,
   },
   SubTitle1: {
-    fontSize: "17px",
+    fontSize: "1.75rem",
     lineHeight: "normal",
     fontWeight: 400,
   },
   SubTitle2: {
-    fontSize: "15px",
+    fontSize: "1.375rem",
+    lineHeight: "normal",
+    fontWeight: 400,
+  },
+  SubTitle3: {
+    fontSize: "1.0625rem",
+    lineHeight: "normal",
+    fontWeight: 400,
+  },
+  SubTitle4: {
+    fontSize: "0.9375rem",
     lineHeight: "normal",
     fontWeight: 400,
   },
   Body1: {
-    fontSize: "16px",
+    fontSize: "1rem",
     lineHeight: "normal",
     fontWeight: 500,
   },
   Body2: {
-    fontSize: "14px",
+    fontSize: "0.875rem",
     lineHeight: "normal",
     fontWeight: 500,
-  },
-  Button: {
-    fontSize: "14px",
-    lineHeight: "normal",
-    fontWeight: 300,
-  },
-  Caption: {
-    fontSize: "12px",
-    lineHeight: "normal",
-    fontWeight: 200,
   },
 };
 
