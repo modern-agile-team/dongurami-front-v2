59a4dff98ba1e226b2a582d5c20100ab4daa14ae
soonki-98 <alstnsrl98@gmail.com>
2024-02-18 18:13:45 +0900

    feat(QYOG-86): add left and right arrow icon

--- src/components/Svg/Icons/index.ts
+++ src/components/Svg/Icons/index.ts
@@ -2,4 +2,6 @@ export { default as Chat24 } from "./chat-24";
 export { default as Check35 } from "./check-35";
 export { default as Heart24 } from "./heart-24";
 export { default as Heartfull24 } from "./heartfull-24";
+export { default as LeftArrow24 } from "./left-arrow-24";
+export { default as RightArrow24 } from "./right-arrow-24";
 export { default as Search30 } from "./search-30";
---
+++ src/components/Svg/Icons/right-arrow-24.tsx
@@ -0,0 +1,48 @@
+import React, { forwardRef, Ref } from "react";
+import { IconProps } from "../Icon";
+import Svg from "../Svg";
+const SvgRightArrow24 = forwardRef(
+  (
+    { size, title, desc, titleId, descId, ...props }: IconProps,
+    ref: Ref<SVGSVGElement>
+  ) => {
+    let ariaLabelledBy: string | undefined = titleId ? titleId : "";
+    ariaLabelledBy += desc && descId ? ` ${descId}` : "";
+    ariaLabelledBy = ariaLabelledBy ? ariaLabelledBy : undefined;
+    props["aria-labelledby"] = ariaLabelledBy;
+    return (
+      <Svg
+        height={size}
+        width={size}
+        id="Layer_1"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+        viewBox="0 0 330 330"
+        xmlSpace="preserve"
+        title={title}
+        titleId={titleId}
+        desc={desc}
+        descId={descId}
+        aria-labelledby={ariaLabelledBy}
+        aria-hidden={!ariaLabelledBy}
+        ref={ref}
+        {...props}
+      >
+        {!!title && <title id={titleId}>{title}</title>}
+        {!!desc && <desc id={descId}>{desc}</desc>}
+        <path
+          id="XMLID_222_"
+          d="M250.606,154.389l-150-149.996c-5.857-5.858-15.355-5.858-21.213,0.001 c-5.857,5.858-5.857,15.355,0.001,21.213l139.393,139.39L79.393,304.394c-5.857,5.858-5.857,15.355,0.001,21.213 C82.322,328.536,86.161,330,90,330s7.678-1.464,10.607-4.394l149.999-150.004c2.814-2.813,4.394-6.628,4.394-10.606 C255,161.018,253.42,157.202,250.606,154.389z"
+        />
+      </Svg>
+    );
+  }
+);
+SvgRightArrow24.defaultProps = {
+  size: 24,
+  focusable: false,
+  "aria-hidden": true,
+  role: "img",
+  fill: "currentcolor",
+};
+export default SvgRightArrow24;
