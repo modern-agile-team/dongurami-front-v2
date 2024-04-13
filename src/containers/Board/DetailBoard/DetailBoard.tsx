d06431833940af59ae2c00523fd96d5219be36f1
jsj1510 <tw450497@gmail.com>
2024-04-13 14:59:39 +0900

    feat(QYOG-125): 일반 게시판 본문, 좋아요 영역 UI

--- src/containers/Board/DetailBoard/DetailBoard.tsx
+++ src/containers/Board/DetailBoard/DetailBoard.tsx
@@ -1,6 +1,6 @@
 import { useRouter } from "next/router";
 import { useQuery, useMutation } from "@tanstack/react-query";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import * as S from "./emotion";
 import { freePostsAPI, noticePostsAPI } from "@/apis";
@@ -8,23 +8,30 @@ import {
   FreePostDetailResponseDto,
   NoticePostDetailResponseDto,
 } from "@/apis/data-contracts";
+import { Column, Row } from "@/components/Layouts";
+import { Typography } from "@/components/Utilities/Typography";
+import { Converter } from "@/utils";
+import { lightThemeColor } from "@/styles/theme";
 
 export default function DetailBoard() {
   const router = useRouter();
   const { postId, type } = router.query;
+
+  const [unixTimestamp, setUnixTimestamp] = useState(0);
 
   const { data } = useQuery({
     queryFn: async () => {
       let response;
       if (type === "free") {
         response = await freePostsAPI.freePostFindOneOrNotFound(Number(postId));
+        return response.data.freePost;
       } else {
         response = await noticePostsAPI.noticePostFindOneOrNotFound(
           Number(postId)
         );
-      }
 
-      return response.data;
+        return response.data.noticePost;
+      }
     },
 
     queryKey: ["post", postId, type],
@@ -52,6 +59,9 @@ export default function DetailBoard() {
   useEffect(() => {
     if (data) {
       mutate();
+
+      const dateObject = new Date(data.createdAt);
+      setUnixTimestamp(dateObject.getTime());
     }
   }, [data, mutate]);
 
@@ -71,31 +81,114 @@ export default function DetailBoard() {
   };
 
   return (
-    <S.Container>
-      <S.Title>
-        {
-          (type === "free"
-            ? (data as FreePostDetailResponseDto)?.freePost
-            : (data as NoticePostDetailResponseDto)?.noticePost
-          )?.title
-        }
-      </S.Title>
-      <S.WrapBar>
-        <S.Btn onClick={handleClickUpdate}>수정</S.Btn>
-        <S.Btn onClick={handleClickDelete}>삭제</S.Btn>
-      </S.WrapBar>
+    <Column
+      horizonAlign="center"
+      style={{
+        width: Converter.pxToRem(787),
+      }}
+      gap={0}
+    >
+      <S.WrapTitle verticalAlign="center" horizonAlign="distribute">
+        <S.Title>
+          <Typography typoSize="Head6" typoColor="accent_100">
+            {data?.title}
+          </Typography>
+        </S.Title>
+
+        <S.Btn
+          color="accent_100"
+          style={{
+            border: `1px solid ${lightThemeColor.accent_100}`,
+          }}
+          onClick={handleClickUpdate}
+        >
+          <Typography typoSize="Body2" typoColor="accent_100">
+            공지로 등록
+          </Typography>
+        </S.Btn>
+      </S.WrapTitle>
+
+      <S.WrapTag horizonAlign="left">
+        <Typography typoSize="Head7" typoColor="neutral_30">
+          #공지 #어쩌구 #저쩌구
+        </Typography>
+      </S.WrapTag>
+
+      <S.Line />
+
+      <Row.ul
+        horizonAlign="right"
+        style={{
+          width: "100%",
+        }}
+      >
+        <Row.li
+          style={{
+            marginRight: 12,
+          }}
+        >
+          <Typography typoSize="Body1" typoColor="neutral_70">
+            유저이름
+          </Typography>
+        </Row.li>
+        <Row.li>
+          <Typography typoSize="Body1" typoColor="neutral_70">
+            {Converter.timestampToDate(unixTimestamp)}
+          </Typography>
+        </Row.li>
+      </Row.ul>
+
       <S.WrapDesc>
         <S.Desc>
-          <S.Title>
-            {
-              (type === "free"
-                ? (data as FreePostDetailResponseDto)?.freePost
-                : (data as NoticePostDetailResponseDto)?.noticePost
-              )?.description
-            }
-          </S.Title>
+          <S.Title>{data?.description}</S.Title>
         </S.Desc>
       </S.WrapDesc>
-    </S.Container>
+
+      <S.WrapBar horizonAlign="distribute" gap={0}>
+        <Row.li>
+          <S.Btn
+            style={{
+              border: `1px solid ${lightThemeColor.accent_100}`,
+              marginRight: 15,
+            }}
+            onClick={handleClickUpdate}
+          >
+            <Typography typoSize="Body1" typoColor="accent_100">
+              좋아요 {data?.hit}개
+            </Typography>
+          </S.Btn>
+          <S.Btn
+            style={{
+              border: `1px solid ${lightThemeColor.accent_100}`,
+            }}
+            onClick={handleClickDelete}
+          >
+            <Typography typoSize="Body1" typoColor="accent_100">
+              댓글 쓰기
+            </Typography>
+          </S.Btn>
+        </Row.li>
+
+        <Row.li>
+          <S.Btn
+            style={{
+              border: `1px solid ${lightThemeColor.neutral_20}`,
+              marginRight: 9,
+            }}
+            onClick={handleClickUpdate}
+          >
+            수정
+          </S.Btn>
+          <S.Btn
+            style={{
+              border: `1px solid ${lightThemeColor.neutral_20}`,
+            }}
+            onClick={handleClickDelete}
+          >
+            삭제
+          </S.Btn>
+        </Row.li>
+      </S.WrapBar>
+    </Column>
   );
 }
--- src/containers/Board/DetailBoard/emotion.ts
+++ src/containers/Board/DetailBoard/emotion.ts
@@ -1,32 +1,45 @@
 import { Button } from "@/components/Design";
 import { Row } from "@/components/Layouts";
+import { lightThemeColor } from "@/styles/theme";
+import { Converter } from "@/utils";
 import styled from "@emotion/styled";
 
-export const Container = styled.div`
-  width: 100vw;
-  height: 100vh;
-  flex-direction: column;
-  background-color: #fff;
+export const WrapTitle = styled(Row.ul)`
+  margin-top: 80px;
+  width: 100%;
 `;
 
-export const Title = styled.h2`
-  padding: 12px 20px;
+export const Title = styled(Row.div)``;
+
+export const WrapTag = styled(Row.ul)`
+  padding: 6px 8px;
+  width: 100%;
+  background-color: ${lightThemeColor.accent_20};
+  margin: 24px 0px;
+  border-radius: 16px;
 `;
 
-export const WrapBar = styled(Row)`
-  padding: 12px 20px;
-  justify-content: flex-end;
+export const Line = styled(Row.div)`
+  width: 100%;
+  height: 1px;
+  background-color: ${lightThemeColor.accent_30};
+  margin-bottom: 10px;
+`;
+
+export const WrapBar = styled(Row.ul)`
+  width: 100%;
+  margin-top: 80px;
 `;
 
 export const Btn = styled(Button)`
-  padding: 6px;
-  margin-right: 12px;
+  padding: 3px 20px;
+  background-color: #fff;
 `;
 
 export const WrapDesc = styled.div`
-  padding: 12px 20px;
-  margin: 12px 20px;
-  border: 1px solid #222;
+  width: 100%;
+  margin-top: 50px;
+  min-height: ${Converter.pxToRem(360)};
 `;
 
 export const Desc = styled.span``;
