af9bd09bf21d5d53301998e60f18bf418a6bfe80
woo <pho9902@naver.com>
2024-04-13 15:22:36 +0900

    feat(QYOG-37): 로그인 모달 스타일 부분 리뷰 반영

--- src/containers/Login/Form/Form.tsx
+++ src/containers/Login/Form/Form.tsx
@@ -16,6 +16,7 @@ import Logo from "@/assets/main/logo_2.png";
 import Google from "@/assets/social/google.png";
 import Naver from "@/assets/social/naver.png";
 import Kakao from "@/assets/social/kakao.png";
+import { Typography } from "@/components/Utilities";
 
 export default function Form() {
   const { data } = useSession();
@@ -87,32 +88,63 @@ export default function Form() {
 
   return (
     <S.Wrap>
-      <S.Left>
+      <S.Left verticalAlign="center" horizonAlign="center">
         <S.LeftLogo src={Logo} alt="LeftLogo" />
       </S.Left>
-      <S.Right>
+      <S.Right verticalAlign="center" horizonAlign="center">
         <S.RightLogo name="Logo" fill="primary_100" />
-        <S.Regist>회원가입</S.Regist>
+        <S.Regist>
+          <Typography typoSize="Head3" typoColor="primary_100">
+            회원가입
+          </Typography>
+        </S.Regist>
         {!accessToken ? (
           <S.ButtonDiv>
-            <S.Button bgColor="#34AD37" onClick={() => signIn("naver")}>
-              <S.ButtonLogoDiv bgColor="#2D843A">
+            <S.Button
+              verticalAlign="center"
+              horizonAlign="center"
+              bgColor="#34AD37"
+              onClick={() => signIn("naver")}
+            >
+              <S.ButtonLogoDiv
+                horizonAlign="center"
+                verticalAlign="center"
+                bgColor="#2D843A"
+              >
                 <S.ButtonLogo src={Naver} alt="naverIcon" />
               </S.ButtonLogoDiv>
               <S.ButtonText textColor="white">
                 네이버 계정으로 가입
               </S.ButtonText>
             </S.Button>
-            <S.Button bgColor="#FFC63A" onClick={() => signIn("kakao")}>
-              <S.ButtonLogoDiv bgColor="#FFBB12">
+            <S.Button
+              horizonAlign="center"
+              verticalAlign="center"
+              bgColor="#FFC63A"
+              onClick={() => signIn("kakao")}
+            >
+              <S.ButtonLogoDiv
+                horizonAlign="center"
+                verticalAlign="center"
+                bgColor="#FFBB12"
+              >
                 <S.ButtonLogo src={Kakao} alt="kakaoIcon" />
               </S.ButtonLogoDiv>
               <S.ButtonText textColor="white">
                 카카오 계정으로 가입
               </S.ButtonText>
             </S.Button>
-            <S.Button bgColor="white" onClick={() => signIn("google")}>
-              <S.ButtonLogoDiv bgColor="white">
+            <S.Button
+              horizonAlign="center"
+              verticalAlign="center"
+              bgColor="white"
+              onClick={() => signIn("google")}
+            >
+              <S.ButtonLogoDiv
+                horizonAlign="center"
+                verticalAlign="center"
+                bgColor="white"
+              >
                 <S.ButtonLogo src={Google} alt="googleIcon" />
               </S.ButtonLogoDiv>
               <S.ButtonText textColor="black">구글 계정으로 가입</S.ButtonText>
--- src/containers/Login/Form/emotion.ts
+++ src/containers/Login/Form/emotion.ts
@@ -10,11 +10,14 @@ import { Row, Column } from "@/components/Layouts";
 
 import Image from "next/image";
 import { Icon } from "@/components/Svg";
+import { Converter } from "@/utils";
+
+const { pxToRem } = Converter;
 
 export const Wrap = styled(Row.div)`
   box-shadow: 10px 10px 10px 0px rgba(0, 0, 0, 0.4);
-  width: 1265px;
-  height: 802px;
+  width: ${pxToRem(1265)};
+  height: ${pxToRem(802)};
   flex-shrink: 0;
   border-radius: 35px;
 `;
@@ -22,12 +25,10 @@ export const Wrap = styled(Row.div)`
 export const Left = styled(Row.div)`
   background: ${({ theme }) => theme.color.primary_100};
   box-shadow: 10px 4px 4px 0px rgba(0, 0, 0, 0.25);
-  width: 554px;
-  height: 802px;
+  width: ${pxToRem(554)};
+  height: ${pxToRem(802)};
   flex-shrink: 0;
   border-radius: 35px 0px 0px 35px;
-  justify-content: center;
-  align-items: center;
 `;
 
 export const LeftLogo = styled(Image)`
@@ -36,26 +37,18 @@ export const LeftLogo = styled(Image)`
 
 export const Right = styled(Column.div)`
   background: white;
-  width: 711px;
-  height: 802px;
+  width: ${pxToRem(711)};
+  height: ${pxToRem(802)};
   flex-shrink: 0;
   border-radius: 0px 35px 35px 0px;
-  align-items: center;
-  justify-content: center;
 `;
 
 export const RightLogo = styled(Icon)`
-  width: 44px;
-  height: 49px;
+  width: ${pxToRem(44)};
+  height: ${pxToRem(49)};
 `;
 
 export const Regist = styled.h3`
-  color: ${({ theme }) => theme.color.primary_100};
-  font-family: "Noto Sans";
-  font-size: 48px;
-  font-style: normal;
-  font-weight: 700;
-  line-height: normal;
   margin: 2% 0 4% 0;
 `;
 
@@ -64,31 +57,29 @@ export const ButtonDiv = styled(Column.div)``;
 export const Button = styled(Row.div)<{ bgColor: string }>`
   cursor: pointer;
   background-color: ${(props) => props.bgColor};
-  width: 350px;
-  height: 60px;
+  width: ${pxToRem(350)};
+  height: ${pxToRem(60)};
   border-radius: 18px;
   margin-bottom: 11%;
-  justify-content: center;
-  align-items: center;
   text-align: center;
 
   border: ${(props) => (props.bgColor === "white" ? "1px solid" : "none")};
 `;
 
-export const ButtonLogoDiv = styled.div<{ bgColor: string }>`
+export const ButtonLogoDiv = styled(Row.div)<{
+  bgColor: string;
+}>`
   background-color: ${(props) => props.bgColor};
   height: 100%;
   display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 85px;
+  width: ${pxToRem(85)};
   border-radius: 18px 0px 0px 18px;
 `;
 export const ButtonLogo = styled(Image)``;
 
 export const ButtonText = styled.span<{ textColor: string }>`
   color: ${(props) => props.textColor};
-  width: 265px;
+  width: ${pxToRem(265)};
   font-family: "Noto Sans";
   font-size: 22px;
   font-style: normal;
